9fc3f1e0af982824e9687f256a1290fa8a873293
Levan <levan.soroznishvili@gmail.com>
2023-12-28 23:57:36 +0400

    application layout

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,27 +1,17 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Posts</title>
-</head>
-<body>
+@extends('layouts.app-layout')
 
-<h1>Personal Posts</h1>
-
-@if($posts->count())
-    <ul>
-        @foreach($posts as $post)
-            <li>
-                <h2><a href="/posts/{{ $post->slug }}">{{ $post->slug }}</a></h2>
-                <div>{{ $post->excerpt }}</div>
-                <div>Posted {{ $post->date->diffForHumans() }} by {{ $post->author }}</div>
-            </li>
-        @endforeach
-    </ul>
-@endif
-
-</body>
-</html>
+@section('content')
+    @if($posts->count())
+        <ul class="list-unstyled">
+            @foreach($posts as $post)
+                <li>
+                    <div class="mb-4">
+                        <h4 class="mb-3"><a href="/posts/{{ $post->slug }}">{{ $post->slug }}</a></h4>
+                        <div>{{ $post->excerpt }}</div>
+                        <div>Posted {{ $post->date->diffForHumans() }} by {{ $post->author }}</div>
+                    </div>
+                </li>
+            @endforeach
+        </ul>
+    @endif
+@endsection
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,17 +1,7 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Posts</title>
-</head>
-<body>
+@extends('layouts.app-layout')
 
-<h1>Post {{ $post->title }}</h1>
-<div>Posted {{ $post->date->diffForHumans() }} by {{ $post->author }}</div>
-<div>{{ $post->contents }}</div>
-
-</body>
-</html>
+@section('content')
+    <h4>{{ $post->title }}</h4>
+    <div>Posted {{ $post->date->diffForHumans() }} by {{ $post->author }}</div>
+    <div>{{ $post->contents }}</div>
+@endsection
